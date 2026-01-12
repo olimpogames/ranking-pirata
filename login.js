3da02c987805af90978b06891b1f0b56c46bb91e
anael1234 <74458952+anael1234@users.noreply.github.com>
2026-01-12 11:18:48 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,21 @@
+function login() {
+  const user = document.getElementById("user").value;
+  const pass = document.getElementById("pass").value;
+  const msg = document.getElementById("msg");
+
+  const USER_OK = "seba";
+  const PASS_OK = "chupalo";
+
+  if (user === USER_OK && pass === PASS_OK) {
+    msg.style.color = "green";
+    msg.innerText = "Acceso permitido...";
+
+    // 👉 Abrir ambas pestañas
+    window.open("pagina1.html", "_blank");
+    window.open("pagina2.html", "_blank");
+
+  } else {
+    msg.style.color = "red";
+    msg.innerText = "Usuario o contraseña incorrectos";
+  }
+}
